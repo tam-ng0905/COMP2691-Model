6dd5401b6a6421d95c6bac4cd5a326a3e3940130
Tam Nguyen <ttamng0905@gmail.com>
2021-02-23 13:35:41 -0700

    finish with addition and subtraction

--- src/BinaryCalculator.java
+++ src/BinaryCalculator.java
@@ -6,13 +6,14 @@
  */
 public class BinaryCalculator
 {
-    public static BitField add(BitField a, BitField b)
-    {
-	if(null == a || null == b || a.size() != b.size()){
+    public static BitField add(BitField A, BitField B) {
+	if(null == A || null == B || A.size() != B.size()){
 	    throw new IllegalArgumentException("BinaryCalculator.add(a,b): a and b cannot be null and must be the same length.");
 	}
 
-	BitField result = new BitField(a.size());
+	BitField result = new BitField(A.size());
+	BitField a = reverse(A);
+	BitField b = reverse(B);
 	boolean carries = false;
 	for(int i = a.size() - 1; i >= 0; i--) {
 		if(a.get(i) && b.get(i)){
@@ -42,11 +43,61 @@ public class BinaryCalculator
 			}
 		}
 	}
-	return reverse(result);
+		return reverse(result);
+    }
+	public static BitField subtract(BitField A, BitField B) {
+	if(null == A || null == B || A.size() != B.size()){
+	    throw new IllegalArgumentException("BinaryCalculator.add(a,b): a and b cannot be null and must be the same length.");
+	}
+		BitField negated = negate(B);
+
+		return add(A, negated);
+    }
+    public static BitField multiply(BitField A, BitField B) {
+		if(null == A || null == B || A.size() != B.size()){
+			throw new IllegalArgumentException("BinaryCalculator.add(a,b): a and b cannot be null and must be the same length.");
+		}
+
+		BitField result = new BitField(A.size());
+		BitField nullBit = new BitField(A.size());
+
+		nullBit.setAllFalse();
+		result.setAllFalse();
+		while(!A.equals(nullBit)){
+			result = add(result, B);
+			A= shiftRight(A);
+			B = shiftLeft(B);
+		}
+		return result;
+    }
+    public static BitField[] divide(BitField a, BitField b) {
+		if(null == a || null == b || a.size() != b.size()){
+			throw new IllegalArgumentException("BinaryCalculator.add(a,b): a and b cannot be null and must be the same length.");
+		}
+		// Return both the quotient and the remainder
+		BitField[] out = new BitField [ 2 ];
+		out[0] = new BitField(a.size()); // quotient
+		out[1] = new BitField(a.size()); // remainder
+		return out;
     }
 
-	static BitField reverse(BitField a)
-	{
+	public static BitField shiftLeft(BitField a){
+		BitField dup = new BitField(a.size());
+		for(int i = 0; i < a.size() - 1; i++){
+			dup.set(i, a.get(i+1));
+		}
+		dup.set(a.size() - 1, false);
+		return reverse(dup);
+	}
+	public static BitField shiftRight(BitField a){
+		BitField dup = new BitField(a.size());
+		dup.set(0, false);
+		for(int i = 1; i < a.size(); i++){
+			dup.set(i, a.get(i-1));
+		}
+		return dup;
+	}
+	public static BitField reverse(BitField a) {
 		BitField b = new BitField(a.size());
 		int j = a.size();
 		for (int i = 0; i < a.size(); i++) {
@@ -55,48 +106,18 @@ public class BinaryCalculator
 		}
 		return b;
 	}
-	public static BitField subtract(BitField a, BitField b)
-    {
-	if(null == a || null == b || a.size() != b.size()){
-	    throw new IllegalArgumentException("BinaryCalculator.add(a,b): a and b cannot be null and must be the same length.");
-	}
-		BitField negated = negate(b);
-
-		return add(a, negated);
-    }
-
-    public static BitField negate(BitField a){
+	public static BitField negate(BitField a){
 		BitField c = complement(a);
 		BitField one = new BitField(a.size());
 		one.set(0, true);
 		return add(c, one);
 	}
 	public static BitField complement(BitField a){
-    	BitField dup = new BitField(a.size());
-    	for(int i = 0; i < a.size(); i++){
-    		dup.set(i, !a.get(i));
+		BitField dup = new BitField(a.size());
+		for(int i = 0; i < a.size(); i++){
+			dup.set(i, !a.get(i));
 		}
-    	return dup;
+		return dup;
 	}
 
-
-    public static BitField multiply(BitField a, BitField b)
-    {
-	if(null == a || null == b || a.size() != b.size()){
-	    throw new IllegalArgumentException("BinaryCalculator.add(a,b): a and b cannot be null and must be the same length.");
-	}
-	return new BitField(a.size());
-    }
-
-    public static BitField[] divide(BitField a, BitField b)
-    {
-	if(null == a || null == b || a.size() != b.size()){
-	    throw new IllegalArgumentException("BinaryCalculator.add(a,b): a and b cannot be null and must be the same length.");
-	}
-	// Return both the quotient and the remainder
-	BitField[] out = new BitField [ 2 ];
-	out[0] = new BitField(a.size()); // quotient
-	out[1] = new BitField(a.size()); // remainder
-	return out;
-    }
 }
